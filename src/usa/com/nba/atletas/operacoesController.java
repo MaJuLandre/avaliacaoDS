be0a4b4bbf931b04e1f11ee78dc94db524adbf29
lab04 <lab04@lab4-03>
2024-09-23 22:19:10 -0300

    Commit 2

--- src/usa/com/nba/atletas/operacoesController.java
+++ src/usa/com/nba/atletas/operacoesController.java
@@ -1,5 +1,27 @@
 package usa.com.nba.atletas;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class operacoesController {
 	
+	@FXML
+	private Button buttoncad;
+	//
+	@FXML
+	private TextField textfieldnom;
+	
+	@FXML
+	private TextField textfieldsob;
+	
+	@FXML
+	private TextField textfieldida;
+	
+	@FXML
+	private TextField textfieldpos;
+	
+	@FXML
+	private TextField textfielequ;
 }
+	
